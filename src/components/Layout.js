a9a4d49823527860e2b1a3f75ab4815cd7bafc0e
Mateus Targino <mateusntargino@gmail.com>
2019-08-19 10:37:18 -0300

    Karla font fix
    Changing from Karla to Karmilla font, Karla doesn't have the cedilla character

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -15,7 +15,7 @@ export default class Body extends React.Component {
                     <meta charset="utf-8"/>
                     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                     <meta name="google" content="notranslate" />
-                    <link href="https://fonts.googleapis.com/css?family=Karla:400,400i,700,700i&display=swap&subset=latin-ext" rel="stylesheet" />
+                    {/* <link href="https://fonts.googleapis.com/css?family=Karla:400,400i,700,700i&display=swap&subset=latin-ext" rel="stylesheet" /> */}
                     <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
                 </Helmet>
                 <div id="page" class="site">
